4dd05d48be37694386834f70e6b869fbd123d3ba
Luthfi Fauzi <faiz.uzi96@gmail.com>
2018-03-30 17:04:14 +0700

    nw
    nwtmplate

---
+++ layanan.php
@@ -0,0 +1,240 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Nadella Wedding</title>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link href="css/animate.min.css" rel="stylesheet"> 
+	<link href="css/animate.css" rel="stylesheet" />
+	<link href="css/prettyPhoto.css" rel="stylesheet"> 
+	<link href="css/style.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/demo.css">
+	<link rel="stylesheet" href="css/footer-distributed-with-address-and-phones.css">
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+	<link href="http://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css">
+	<link href="css/new.css" rel="stylesheet">
+	<link rel='stylesheet prefetch' href='css_tabel/egkepq.css'>
+    <link rel="stylesheet" href="css_tabel/style.css">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+  	 <div id="home">
+		<div class="slider">
+			<div id="about-slider">
+				<div id="carousel-slider" class="carousel slide" data-ride="carousel">
+					<!-- Indicators -->
+					<ol class="carousel-indicators visible-xs">
+						<li data-target="#carousel-slider" data-slide-to="0" class="active"></li>
+						<li data-target="#carousel-slider" data-slide-to="1"></li>
+						<li data-target="#carousel-slider" data-slide-to="2"></li>
+					</ol>	
+
+				</div> <!--/#carousel-slider-->
+			</div><!--/#about-slider-->
+		</div>
+	</div>
+	
+	 <div class="carousel-item active">
+                <img class="img-responsive" src="images/portfolio/title/1.png" alt="First slide" height="600">
+                <div class="judul1">LAYANAN</div></br>
+                <a class="judul2" href="index.php">Halaman</a>
+                <div class="judul3"> / Layanan</div>
+                </div>
+              </div>
+
+	<nav class="navbar navbar-default navbar-fixed-top">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-2">
+					<div class="site-logo">
+						<a href="index.html" class="brand">NadellaWedding</a>
+					</div>
+				</div>					  
+				<div class="col-md-10">	 
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#menu">
+							<i class="fa fa-bars"></i>
+						</button>
+					</div>
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse" id="menu">
+						<ul class="nav navbar-nav navbar-right">
+							  <li><a href="index.php">Halaman</a></li>
+							  <li><a href="portofolio.php">Portfolio</a></li>
+							  <li><a href="layanan.php">Layanan</a></li>
+							  <li><a href="kontak.php">Kontak</a></li>
+							  <li><a href="login.php">Login</a></li>
+						</ul>
+					</div>
+					<!-- /.Navbar-collapse -->		 
+				</div>
+			</div>
+		</div>		
+	</nav>
+<div class="table">
+  <div class="table-cell"></div>
+  <div class="table-cell plattform">
+    <h3>GOLD</h3>
+    <a href="" class="btn">Booking Sekarang</a>
+  </div>
+  <div class="table-cell enterprise">
+    <h3>Platinum</h3>
+    <a href="" class="btn">Booking Sekarang</a>
+  </div>
+  <div class="table-cell cell-feature"><b>Dekorasi Pelaminan Rumah</b></br>
+  Disesuaikan dengan Luas Rumah</br>Termasuk: Pergola Pintu Masuk, Pelaminan dan Taman, Galeri Foto dan Pilar Jalan	</div>
+  <div class="table-cell">
+    <a>± Rp 5.000.000,00</a>
+  </div>
+  <div class="table-cell">
+      <a>± Rp 7.000.000,00</a>
+  </div>
+  <div class="table-cell cell-feature"><b>Dekorasi Pelaminan Gedung</b></br>
+  Disesuaikan dengan Luas Gedung</br>Termasuk: Pergola Pintu Masuk, Galeri Foto, Photo Booth, Pelaminan dan Taman, Meja VIP, Pilar Jalan dan Janur</div>
+  <div class="table-cell">
+    <a>± Rp 10.000.000,00</a>
+  </div>
+  <div class="table-cell">
+      <a>± Rp 16.000.000,00</a>
+  </div>
+  <div class="table-cell cell-feature"><b>Rias Pengantin</b>
+  </div>
+  <div class="table-cell">
+    <a>± Rp 7.000.000,00</br>
+    Baju Pengantin Mempelai Pria, Baju Pengantin Mempelai Wanita, Baju Orang Tua, Pagar Ayu (4), Pagar Bagus (2)</a>
+  </div>
+  <div class="table-cell">
+      <a>± Rp 10.000.000,00</br>
+      Paket Platinum, termasuk: Baju Pengantin Mempelai Pria (2), Baju Pengantin Mempelai Wanita (2), Baju Orang Tua, Baju Pagar Ayu (4), Baju Pagar Bagus (2)</a>
+</div>
+    <div class="table-cell cell-feature">Photography and Video Shooting</div>
+  <div class="table-cell">
+    <a>± Rp 5.000.000,00</a>
+  </div>
+  <div class="table-cell">
+      <a>± Rp 9.000.000,00</a>
+  </div>
+  <div class="table-cell cell-feature">MC Acara</div>
+  <div class="table-cell">
+    <a>± Rp 1.000.000,00</a>
+  </div>
+  <div class="table-cell">
+      <a>± Rp 2.000.000,00</a>
+  </div>
+  <div class="table-cell cell-feature">Music Organ Tunggal</div>
+  <div class="table-cell">
+    <a>± Rp 2.000.000,00</a>
+  </div>
+  <div class="table-cell">
+      <a>± Rp 2.500.000,00</a>
+  </div>
+  <div class="table-cell cell-feature">Musik Nasyid, Pop</div>
+  <div class="table-cell">
+    <a>± Rp 2.500.000,00</a>
+  </div>
+  <div class="table-cell">
+      <a>± Rp 3.000.000,00</a>
+  </div>
+  <div class="table-cell cell-feature">Upacara Adat dan Lengser</div>
+  <div class="table-cell">
+    <a>± Rp 4.000.000,00</a>
+  </div>
+  <div class="table-cell">
+      <a>± Rp 5.000.000,00</a>
+  </div>
+  <div class="table-cell cell-feature">Siraman</div>
+  <div class="table-cell">
+    <a>± Rp 3.000.000,00</a>
+  </div>
+  <div class="table-cell">
+      <a>± Rp 3.500.000,00</a>
+  </div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+ <footer class="footer-distributed">
+			<div class="footer-left">
+				<h1 class="judulfoot"><b>NadellaWedding</b></h1>
+				<p class="footer-links">
+					<a href="#">Halaman</a>
+					·
+					<a href="#">Portofolio</a>
+					·
+					<a href="#">Layanan</a>
+					·
+					<a href="#">Kontak</a>
+					·
+					<a href="#">Login</a>
+					·
+				</p>
+				<p class="footer-company-name">Luthfifauzi &copy; 2015</p>
+			</div>
+			<div class="footer-center">
+				<div>
+					<i class="fa fa-map-marker"></i>
+					<p><span>Jl.Batujajar, Perum Laksana Mekar Asri Blok B.42</span>
+					Padalarang - Bandung Barat</p>
+				</div>
+
+				<div>
+					<i class="fa fa-phone"></i>
+					<p >+62 856-2201-690</p>
+				</div>
+
+				<div>
+					<i class="fa fa-envelope"></i>
+					<p>nadelawedding@gmail.com</p>
+				</div>
+
+			</div>
+
+			<div class="footer-right">
+
+				<p class="footer-company-about">
+					<span>Nadella Wedding</span>
+					Wedding Organizer yang menyediakan fasilitas rias pengantin, dekorasi dan tata lokasi, siraman adat istiadat juga dokumentasi handal
+				</p>
+
+				<div class="footer-icons">
+
+					<a href="https://web.facebook.com/NadelaSalon/"><i class="fa fa-facebook"></i></a>
+					<a href="#"><i class="fa fa-instagram"></i></a>
+					<a href="https://api.whatsapp.com/send?phone=628562201690"><i class="fa fa-phone"></i></a>
+					<a href="#"><i class="fa fa-github"></i></a>
+
+				</div>
+
+			</div>
+
+		</footer>
+	
+	
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<script src="js/jquery.js"></script>
+	<!-- Include all compiled plugins (below), or include individual files as needed -->
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.prettyPhoto.js"></script>
+    <script src="js/jquery.isotope.min.js"></script> 
+	<script src="js/wow.min.js"></script>
+	<script src="js/jquery.easing.min.js"></script>	
+	<script src="js/main.js"></script>
+  </body>
+</html>
---
+++ portofolio.php
@@ -0,0 +1,272 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- HEADER JANGAN DIHAPUS-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- TITLE JANGAN DIHAPUS-->
+    <title>Nadella Wedding</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link href="css/animate.min.css" rel="stylesheet"> 
+	<link href="css/animate.css" rel="stylesheet" />
+	<link href="css/prettyPhoto.css" rel="stylesheet"> 
+	<link href="css/style.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/demo.css">
+	<link rel="stylesheet" href="css/footer-distributed-with-address-and-phones.css">
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+	<link href="http://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css">
+	<link href="css/new.css" rel="stylesheet">
+  </head>
+
+<!-- BADANKU JANGAN DIHAPUS-->
+  <body>
+  	 <div id="home">
+		<div class="slider">
+			<div id="about-slider">
+				<div id="carousel-slider" class="carousel slide" data-ride="carousel">
+					<!-- Indicators -->
+					<ol class="carousel-indicators visible-xs">
+						<li data-target="#carousel-slider" data-slide-to="0" class="active"></li>
+						<li data-target="#carousel-slider" data-slide-to="1"></li>
+						<li data-target="#carousel-slider" data-slide-to="2"></li>
+					</ol>	
+				</div> <!--/#carousel-slider-->
+			</div><!--/#about-slider-->
+		</div>
+	</div>
+	
+	 <div class="carousel-item active">
+                <img class="img-responsive" src="images/portfolio/title/1.png" alt="First slide" height="600">
+                <div class="judul1">PORTOFOLIO</div></br>
+                <a class="judul2" href="index.php">Halaman</a>
+                <div class="judul3"> / Portofolio</div>
+                </div>
+              </div>
+
+	<nav class="navbar navbar-default navbar-fixed-top">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-2">
+					<div class="site-logo">
+						<a href="index.html" class="brand">NadellaWedding</a>
+					</div>
+				</div>					  
+				<div class="col-md-10">	 
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#menu">
+							<i class="fa fa-bars"></i>
+						</button>
+					</div>
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse" id="menu">
+						<ul class="nav navbar-nav navbar-right">
+							  <li><a href="index.php">Halaman</a></li>
+							  <li><a href="portofolio.php">Portfolio</a></li>
+							  <li><a href="layanan.php">Layanan</a></li>
+							  <li><a href="kontak.php">Kontak</a></li>
+							  <li><a href="login.php">Login</a></li>
+						</ul>
+					</div>
+					<!-- /.Navbar-collapse -->		 
+				</div>
+			</div>
+		</div>		
+	</nav>
+	        <div class="container">
+			<div class="col-lg-12">		
+				<ul class="portfolio-filter text-center">
+					<li><a class="btn btn-default active" href="#" data-filter="*">
+					Semua Kegiatan</a></li>
+					<li><a class="btn btn-default" href="#" data-filter=".bootstrap">
+					Rias Pengantin</a></li>
+					<li><a class="btn btn-default" href="#" data-filter=".html">
+					Dekorasi Lokasi</a></li>
+					<li><a class="btn btn-default" href="#" data-filter=".wordpress">
+					Make Up</a></li>
+				</ul><!--/#portfolio-filter-->
+
+				<div class="row">
+					<div class="portfolio-items">
+						<div class="portfolio-item apps col-xs-12 col-sm-4 col-md-3">
+							<div class="recent-work-wrap">
+								<img class="img-responsive" src="images/portfolio/recent/item01.png" alt="">
+								<div class="overlay">
+									<div class="recent-work-inner">
+										<h3><a href="#">DEKORASI</a></h3>
+										<p>Salah satu contoh dekorasi pada rias pengantin. Klik <a href="galeri.php">Galeri </a> untuk melihat lebih banyak
+										</p>
+										<a class="preview" href="images/portfolio/full/item01.png" rel="prettyPhoto"><i class="fa fa-eye"></i> Lihat</a>
+									</div> 
+								</div>
+							</div>
+						</div><!--/.portfolio-item-->
+
+						<div class="portfolio-item joomla bootstrap col-xs-12 col-sm-4 col-md-3">
+							<div class="recent-work-wrap">
+								<img class="img-responsive" src="images/portfolio/recent/item02.png" alt="">
+								<div class="overlay">
+									<div class="recent-work-inner">
+										<h3><a href="#">RIAS PENGANTIN</a></h3>
+										<p>Salah satu contoh model rias pengantin. Klik <a href="galeri.php">Galeri </a> untuk melihat lebih banyak</p>
+										<a class="preview" href="images/portfolio/full/item02.png" rel="prettyPhoto"><i class="fa fa-eye"></i> Lihat</a>
+									</div> 
+								</div>
+							</div>          
+						</div><!--/.portfolio-item-->
+
+						<div class="portfolio-item bootstrap wordpress col-xs-12 col-sm-4 col-md-3">
+							<div class="recent-work-wrap">
+								<img class="img-responsive" src="images/portfolio/recent/item03.png" alt="">
+								<div class="overlay">
+									<div class="recent-work-inner">
+										<h3><a href="#">RIAS PENGANTIN</a></h3>
+										<p>Salah satu contoh model rias pengantin. Klik <a href="galeri.php">Galeri </a> untuk melihat lebih banyak</p>
+										<a class="preview" href="images/portfolio/full/item03.png" rel="prettyPhoto"><i class="fa fa-eye"></i> Lihat</a>
+									</div> 
+								</div>
+							</div>        
+						</div><!--/.portfolio-item-->
+
+						<div class="portfolio-item joomla wordpress apps col-xs-12 col-sm-4 col-md-3">
+							<div class="recent-work-wrap">
+								<img class="img-responsive" src="images/portfolio/recent/item04.png" alt="">
+								<div class="overlay">
+									<div class="recent-work-inner">
+										<h3><a href="#">MAKE UP WISUDA</a></h3>
+										<p>Salah satu contoh model make up wisuda. Klik <a href="galeri.php">Galeri </a> untuk melihat lebih banyak</p>
+										<a class="preview" href="images/portfolio/full/item04.png" rel="prettyPhoto"><i class="fa fa-eye"></i> Lihat</a>
+									</div> 
+								</div>
+							</div>           
+						</div><!--/.portfolio-item-->
+			  
+						<div class="portfolio-item joomla html bootstrap col-xs-12 col-sm-4 col-md-3">
+							<div class="recent-work-wrap">
+								<img class="img-responsive" src="images/portfolio/recent/item005.png" alt="">
+								<div class="overlay">
+									<div class="recent-work-inner">
+										<h3><a href="#">DEKORASI</a></h3>
+										<p>Salah satu contoh model make up wisuda. Klik <a href="galeri.php">Galeri </a> untuk melihat lebih banyak</p>
+										<a class="preview" href="images/portfolio/full/item005.png" rel="prettyPhoto"><i class="fa fa-eye"></i> Lihat</a>
+									</div> 
+								</div>
+							</div>      
+						</div><!--/.portfolio-item-->
+
+						<div class="portfolio-item wordpress html apps col-xs-12 col-sm-4 col-md-3">
+							<div class="recent-work-wrap">
+								<img class="img-responsive" src="images/portfolio/recent/item06.png" alt="">
+								<div class="overlay">
+									<div class="recent-work-inner">
+										<h3><a href="#">SIRAMAN</a></h3>
+										<p>Salah satu adat pernikahan di Jawa Barat. Klik <a href="galeri.php">Galeri </a> untuk melihat lebih banyak</p>
+										<a class="preview" href="images/portfolio/full/item06.png" rel="prettyPhoto"><i class="fa fa-eye"></i> Lihat</a>
+									</div> 
+								</div>
+							</div>         
+						</div><!--/.portfolio-item-->
+
+						<div class="portfolio-item wordpress html col-xs-12 col-sm-4 col-md-3">
+							<div class="recent-work-wrap">
+								<img class="img-responsive" src="images/portfolio/recent/item07.png" alt="">
+								<div class="overlay">
+									<div class="recent-work-inner">
+										<h3><a href="#">DEKORASI</a></h3>
+										<p>Salah satu contoh model make up wisuda. Klik <a href="galeri.php">Galeri </a> untuk melihat lebih banyak</p>
+										<a class="preview" href="images/portfolio/full/item07.png" rel="prettyPhoto"><i class="fa fa-eye"></i> Lihat</a>
+									</div> 
+								</div>
+							</div>          
+						</div><!--/.portfolio-item-->
+
+						<div class="portfolio-item wordpress html bootstrap col-xs-12 col-sm-4 col-md-3">
+							<div class="recent-work-wrap">
+								<img class="img-responsive" src="images/portfolio/recent/item08.png" alt="">
+								<div class="overlay">
+									<div class="recent-work-inner">
+										<h3><a href="#">SIRAMAN</a></h3>
+										<p>Salah satu adat pernikahan di Jawa Barat. Klik <a href="galeri.php">Galeri </a> untuk melihat lebih banyak</p>
+										<a class="preview" href="images/portfolio/full/item08.png" rel="prettyPhoto"><i class="fa fa-eye"></i> Lihat</a>
+									</div> 
+								</div>
+							</div>          
+						</div><!--/.portfolio-item-->
+					</div>
+				</div>
+			</div>
+		</div>
+    </section><!--/#portfolio-item-->
+
+<footer class="footer-distributed">
+			<div class="footer-left">
+				<h1 class="judulfoot"><b>NadellaWedding</b></h1>
+				<p class="footer-links">
+					<a href="#">Halaman</a>
+					·
+					<a href="#">Portofolio</a>
+					·
+					<a href="#">Layanan</a>
+					·
+					<a href="#">Kontak</a>
+					·
+					<a href="#">Login</a>
+					·
+				</p>
+				<p class="footer-company-name">Luthfifauzi &copy; 2015</p>
+			</div>
+			<div class="footer-center">
+				<div>
+					<i class="fa fa-map-marker"></i>
+					<p><span>Jl.Batujajar, Perum Laksana Mekar Asri Blok B.42</span>
+					Padalarang - Bandung Barat</p>
+				</div>
+
+				<div>
+					<i class="fa fa-phone"></i>
+					<p >+62 856-2201-690</p>
+				</div>
+
+				<div>
+					<i class="fa fa-envelope"></i>
+					<p>nadelawedding@gmail.com</p>
+				</div>
+
+			</div>
+
+			<div class="footer-right">
+
+				<p class="footer-company-about">
+					<span>Nadella Wedding</span>
+					Wedding Organizer yang menyediakan fasilitas rias pengantin, dekorasi dan tata lokasi, siraman adat istiadat juga dokumentasi handal
+				</p>
+
+				<div class="footer-icons">
+
+					<a href="https://web.facebook.com/NadelaSalon/"><i class="fa fa-facebook"></i></a>
+					<a href="#"><i class="fa fa-instagram"></i></a>
+					<a href="https://api.whatsapp.com/send?phone=628562201690"><i class="fa fa-phone"></i></a>
+					<a href="#"><i class="fa fa-github"></i></a>
+
+				</div>
+
+			</div>
+
+		</footer>
+	
+	
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<script src="js/jquery.js"></script>
+	<!-- Include all compiled plugins (below), or include individual files as needed -->
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.prettyPhoto.js"></script>
+    <script src="js/jquery.isotope.min.js"></script> 
+	<script src="js/wow.min.js"></script>
+	<script src="js/jquery.easing.min.js"></script>	
+	<script src="js/main.js"></script>
+  </body>
+</html>
